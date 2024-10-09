6334ce246e0fc6021701dc293c0dbfa0a75bc896
Haidhi Angkawijana <haidhiangkawijana@gmail.com>
2024-10-09 12:46:21 +0000

    add y axis

--- grid_block.cu
+++ grid_block.cu
@@ -9,8 +9,12 @@ __global__ void helloFromGPU()
 
 int main() {
     
-    dim3 grid(8); 
-    dim3 block(4);
+    int nx, ny; 
+    nx = 16; 
+    ny = 4;
+
+    dim3 block(8, 2); // 8 threads in x axis, 2 thread in y axis
+    dim3 grid(nx/block.x, ny/block.y); // 2 block in x axis, 2 block in y axis
 
     helloFromGPU<<<grid, block>>>();
     
